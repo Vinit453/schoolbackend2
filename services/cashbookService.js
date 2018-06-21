81044efc4e2c3f325f820ea89b9d8631b15746c9
narendra_nk <kalekar.narendra@gmail.com>
2018-06-21 23:04:36 +0530

    create new cashbook

--- services/cashbookService.js
+++ services/cashbookService.js
@@ -14,3 +14,35 @@ exports.getCashbooks = function (queryString, callback) {
         }
     });
 }
+
+exports.addCashbook = function (cashbook, callback) {
+    var setString = "";
+    var keyString = "";
+    var valueString = "";
+    let cashbookObjectLeng = Object.keys(cashbook).length;
+    var i = 1;
+    for (var key in cashbook) {
+        if (i != cashbookObjectLeng) {
+            keyString = keyString + key + ',';
+            valueString = valueString + "'" + cashbook[key] + "',";
+        }
+        else {
+            keyString = keyString + key;
+            valueString = valueString + "'" + cashbook[key] + "'";
+        }
+        i++;
+    }
+
+    var sql = "INSERT INTO tbl_cashbook (" + keyString + ") VALUES (" + valueString + ")";
+
+    con.query(sql, function (err, result) {
+        if (err) {
+            callback(null, err);
+            return;
+        }
+        else {
+            callback(null, result);
+            return;
+        }
+    });
+}
