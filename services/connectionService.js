bb52dd3df7254d05af8305b56ba1b389ee17319c
prasad mhatre <prasad8mhatre@gmail.com>
2018-06-24 14:30:11 +0530

    added heroku cleardb connection

--- services/connectionService.js
+++ services/connectionService.js
@@ -1,9 +1,9 @@
 var mysql = require('mysql');
 var connection = mysql.createConnection({
-    host: '127.0.0.1',
-    user: 'root',
-    password: 'root',
-    database: 'schoolmanagement'
+    host: 'us-cdbr-iron-east-04.cleardb.net',
+    user: 'bab1397f63d408',
+    password: '0ed0be87',
+    database: 'heroku_7a47b378a7afcca'
 });
 
 connection.connect(function(err) {
